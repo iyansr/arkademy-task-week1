134398924a626f380792f2eb7d4b7b4b6ee00714
IyanSR <ikhyan7@gmail.com>
2019-10-22 12:51:07 +0700

    Add printerMachine

---
+++ task3/printerMachine.js
@@ -0,0 +1,20 @@
+const alphabet = ['q', 'w', 'z', 'x', 'e', 'r'];
+const command = [true, false, false, true, false, true, true];
+
+const printerMachine = (alpha, cmd) => {
+  let ar = [];
+  if (alpha.length !== cmd.length) {
+    console.log(
+      'Alphabet array lengt should equals to Command array length 😉 '
+    );
+  } else {
+    for (let i = 0; i < alpha.length; i++) {
+      if (cmd[i] === true) {
+        ar.push(alpha[i]);
+      }
+    }
+    console.log(ar);
+  }
+};
+
+printerMachine(alphabet, command);
