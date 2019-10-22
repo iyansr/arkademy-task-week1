90e3c62d611a2f8fc8a7dd1bf9c001d8678eb7dc
IyanSR <ikhyan7@gmail.com>
2019-10-22 20:31:44 +0700

    Fix Simple Math

--- task2/simpleMath.js
+++ task2/simpleMath.js
@@ -1,7 +1,7 @@
 let startMoney = 50000;
 let noodlePrice = 2500;
 
-let temporaryNoodle = 50000 / 2500;
+let temporaryNoodle = startMoney / noodlePrice;
 let bonus = temporaryNoodle / 4;
 
 let totalNoodle = temporaryNoodle + bonus;
