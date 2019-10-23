c40663b5d6f5fbf6008d09e637cc3c2d5c4b5bcf
IyanSR <ikhyan7@gmail.com>
2019-10-23 10:58:15 +0700

    Task 4 number 1 done

--- task4/arrayOperation.js
+++ task4/arrayOperation.js
@@ -1,18 +1,15 @@
-const arrayOperation = () => {
-  let numbers = [123, 9, 27];
-  let totalAll = 0;
-  let n = [];
-  if (numbers.length === 1) {
-    return (n = [numbers[0]]);
-  } else {
-    for (let i = 0; i < numbers.length; i++) {
-      //
-      totalAll += numbers[i];
-      n = ('' + totalAll).split('').map(Number);
-    }
-    return n;
-    // numbers = n;
+let numbers = [123, 9, 27];
+
+const arrayOperation = array => {
+  let ar = array;
+  while (ar.length > 1) {
+    let num = 0;
+    ar.map((v, i) => {
+      num += v;
+    });
+    ar = `${num}`.split('').map(Number);
   }
+  console.log(ar);
 };
 
-console.log(arrayOperation());
+arrayOperation(numbers);
